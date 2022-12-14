53e3eef4c798c236f900db4bc3eae0f7ef68f6d9
Basil Tkachenko <baziltkachenko@gmail.com>
2022-12-14 19:01:19 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import streamlit as st
+
+from lab_01 import lab1_1
+from lab_02 import lab2_3
+from nirs import nir
+
+
+# st.set_page_config(initial_sidebar_state="collapsed")
+st.sidebar.image('logo.png', width=300)
+
+def header():
+    author = '''
+    made by [Basil Tkachenko](https://github.com/Basilt69)
+    in [BMSTU](https://bmstu.ru)
+    '''
+
+    st.header('МГТУ им. Баумана, Кафедра ИУ7')
+    st.markdown("**Курс:** Моделирование")
+    st.markdown("**Преподаватель:** Градов В.М.")
+    st.markdown("**Студент:** Ткаченко В.М.")
+    st.sidebar.markdown(author)
+
+def main():
+    header()
+    lab = st.sidebar.radio(
+        "Выберите лабораторную работу:", (
+            "1. Полиномиальная интерполяция табличных функций(Полином Ньютона).",
+            "2. Алгоритм наилучшего среднеквадратичного приближения.",
+            "3. Численное интегрирование.",
+            "4. Численное дифференцирование.",
+            "5. НИР(Метод Монте-Карло).",
+        ),
+        index=4
+    )
+
+    if lab[:1] == "1":
+        lab1_1.main()
+
+    '''elif lab[:1] == "2":
+        lab2_3.main()
+
+    elif lab[:1] == "3":
+        lab_3.main()
+
+    elif lab[:1] == "4":
+        lab_4.main()
+
+    elif lab[:1] == "5":
+        nir.main()'''
+
+if __name__ == "__main__":
+    main()
