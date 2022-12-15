36eb0e640200c8fdfbbd094e138381622ed5c1b2
Basil Tkachenko <baziltkachenko@gmail.com>
2022-12-15 21:15:57 +0300

    commit v.11(lab1_1 initial data z added)

--- lab_01/lab1_1.py
+++ lab_01/lab1_1.py
@@ -20,12 +20,12 @@ def main():
 
     st.write("Параметры функции:")
 
-    a1, a2, a3 = st.columns(3)
-    b1, b2, b3 = st.columns(3)
-    c1, c2, c3 = st.columns(3)
-    d1, d2, d3 = st.columns(3)
-    e1, e2, e3 = st.columns(3)
-    f1, f2, f3 = st.columns(3)
+    a1, a2, a3, a4 = st.columns(4)
+    b1, b2, b3, b4 = st.columns(4)
+    c1, c2, c3, c4 = st.columns(4)
+    d1, d2, d3, d4 = st.columns(4)
+    e1, e2, e3, e4 = st.columns(4)
+    f1, f2, f3, f4 = st.columns(4)
 
     x1 = a1.number_input("x1", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
     x2 = b1.number_input("x2", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
@@ -41,6 +41,13 @@ def main():
     y5 = e2.number_input("y5", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
     y6 = f2.number_input("y6", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
 
+    z1 = a3.number_input("z1", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
+    z2 = b3.number_input("z2", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
+    z3 = c3.number_input("z3", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
+    z4 = d3.number_input("z4", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
+    z5 = e3.number_input("z5", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
+    z6 = f3.number_input("z6", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
+
     p1 = a3.number_input("p1", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
     p2 = b3.number_input("p2", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
     p3 = c3.number_input("p3", min_value=1.0, max_value=10.0, value=1.0, step=0.25)
@@ -50,12 +57,14 @@ def main():
 
     x_array = [x1, x2, x3, x4, x5, x6]
     y_array = [y1, y2, y3, y4, y5, y6]
+    z_array = [z1, z2, z3, z4, z5, z6]
     p_array = [p1, p2, p3, p4, p5, p6]
     st.subheader("Таблица функции с количеством узлов N:")
 
     data = {
         "x":x_array,
         "y":y_array,
+        "z":z_array,
         "p":p_array,
     }
 
