dd282f43e96f268917faa1b3422713e52ec528dd
Basil Tkachenko <baziltkachenko@gmail.com>
2022-12-16 14:38:43 +0300

    commit v.16_14(scatter data added)

--- lab_01/lab1_1.py
+++ lab_01/lab1_1.py
@@ -21,7 +21,7 @@ def wls_2_dimensional(data, n):
 def plot(data):
     """ отрисовка графика """
     st.markdown("---")
-    fig, ax = plt.subplots(111)
+    fig, ax = plt.subplots()
     data.plot.scatter(x="x", y="y", ax=ax)
     st.pyplot(fig)
     st.markdown("---")
